0d9bda4c90f1c5428df7969e58638e90d36bf88c
Matheus Rocha <matheus_rocha98@outlook.com>
2020-11-21 02:10:59 -0300

    configurando plugin vuex-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,17 @@
 // https://github.com/jsbroks/vuex-typescript-todoapp/blob/master/src/store/index.ts
 import { createStore, createLogger } from 'vuex'
+import { VuexPersistence } from 'vuex-persist'
 import { State, state } from './state'
 import { mutations } from './mutations'
 import { actions } from './actions'
 
+const { plugin: persistencePlugin } = new VuexPersistence({
+  key: '@trampoz',
+  storage: window.localStorage
+})
+
 export const store = createStore<State>({
-  plugins: [createLogger()],
+  plugins: [persistencePlugin, createLogger()],
   state,
   mutations,
   actions
