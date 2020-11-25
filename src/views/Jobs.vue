4b7020405041a10a182b6c980113b18505931439
Matheus Rocha <matheus_rocha98@outlook.com>
2020-11-25 19:51:26 -0300

    paginando tabela de jobs + iniciando dialogs

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -4,19 +4,19 @@
       <template v-slot:content>
         <Toolbar>
           <template v-slot:left>
-            <Button label="Reload" icon="pi pi-refresh" class="p-mr-2 p-button-secondary" @click="fetchJobs()" />
+            <Button label="Reload" icon="pi pi-refresh" class="p-mr-2 p-button-info" @click="fetchJobs()" />
             <Button label="New" icon="pi pi-plus" />
           </template>
 
           <template v-slot:right>
-            <span class="p-mr-1">Page: {{ page }} of {{ pages}}</span>
+            <span class="p-mr-1">Page: {{ page }} of {{ pages }}</span>
             <div class="p-mx-3 p-d-flex p-jc-between" style="width: 5.25rem">
               <Button icon="pi pi-chevron-left" class="p-button-raised p-button-rounded p-button-secondary" @click="goToPeviousPage()" :disabled="!hasPreviousPage" />
               <Button icon="pi pi-chevron-right" class="p-button-raised p-button-rounded p-button-secondary" @click="goToNextPage()" :disabled="!hasNextPage" />
             </div>
             <div>
               <span class="p-d-block">Items:</span>
-              <Dropdown v-model="limit" :options="limitOptions" optionLabel="value" optionValue="value" @change="fetchJobs()" de />
+              <Dropdown v-model="limit" :options="limitOptions" optionLabel="value" optionValue="value" @change="fetchJobs()" />
             </div>
           </template>
         </Toolbar>
@@ -33,25 +33,39 @@
           <Column field="description" header="Description"></Column>
           <Column header="Actions" >
             <template #body="slotProps">
-                <Button icon="pi pi-pencil" class="p-button-rounded p-button-warning p-mr-2" @click="editProduct(slotProps.data)" />
-                <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="confirmDeleteProduct(slotProps.data)" />
+                <Button icon="pi pi-pencil" class="p-button-rounded p-button-warning p-mr-2" @click="editJob(slotProps.data)" />
+                <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="openDeleteDialog(slotProps.data)" />
             </template>
           </Column>
 
           <template #empty>
-            No records found.
+            <p>No records found... ¯\_(ツ)_/¯</p>
           </template>
         </DataTable>
       </template>
     </Card>
+
+    <Dialog :visible="dialog.isDeleteOpen" :closable="false" header="Delete confirmation" :modal="true">
+      <div class="confirmation-content">
+        <i class="pi pi-exclamation-triangle p-mr-3" style="font-size: 1rem" />
+        <span>{{ dialog.job.title }}</span>
+        <span v-if="dialog.job">Are you sure?</span>
+      </div>
+      <template #footer>
+        <Button label="No" icon="pi pi-times" class="p-button-text" @click="dialog.isDeleteOpen = false"/>
+        <Button label="Yes" icon="pi pi-check" class="p-button-text" @click="console.log('zap')" />
+      </template>
+      </Dialog>
   </section>
 </template>
 
 <script lang="ts">
 import { computed } from 'vue'
 import { Vue } from 'vue-class-component'
+import { get, assign } from 'lodash'
 import { useStore } from '@/store'
 import { ActionTypes } from '@/store/actions'
+import { Job } from '@/store/state'
 
 export default class Jobs extends Vue {
   public store = useStore()
@@ -72,6 +86,13 @@ export default class Jobs extends Vue {
     { value: 20 }
   ]
 
+  public dialog = {
+    isCreationOpen: false,
+    isEditOpen: false,
+    isDeleteOpen: false,
+    job: {} as Job
+  }
+
   public async fetchJobs () {
     const paginationParams = { page: this.page, limit: this.limit }
     await this.store.dispatch(ActionTypes.FetchJobs, paginationParams)
@@ -89,6 +110,14 @@ export default class Jobs extends Vue {
   async goToPeviousPage () {
     this.page--
     await this.fetchJobs()
+  }
+
+  openDeleteDialog (job: Job) {
+    const id = get(job, 'id')
+    const title = get(job, 'title')
+
+    assign(this.dialog.job, id, title)
+    this.dialog.isDeleteOpen = true
   }
 }
 </script>
